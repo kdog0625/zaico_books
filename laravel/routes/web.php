b7842c13e6fbd003c05f6c143702ffc23f0bf425
kdog0625 <kdoooog34@gmail.com>
2021-02-20 22:06:28 +0900

    投稿関連ファイル追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'TweetController@index')->name('tweets.index');
