01cea3d6af920d09b79373b2a75b9d477ba9076d
kdog0625 <kdoooog34@gmail.com>
2021-02-21 18:37:49 +0900

    ユーザー認証のビューを追加

--- laravel/resources/views/common/header.blade.php
+++ laravel/resources/views/common/header.blade.php
@@ -14,7 +14,7 @@
 
     @guest 
     <li class="nav-item user_interval">
-      <a class="nav-link" href=""><i class="fas fa-sign-in-alt mr-1"></i>ログイン</a>
+      <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt mr-1"></i>ログイン</a>
     </li>
     @endguest
 
@@ -23,6 +23,28 @@
         <a class="nav-link bg-guest" href=""><i class="fas fa-user-check mr-1"></i>ゲストログイン</a>
       </li>
     @endguest
+
+    @auth
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+         aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-user-circle"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+        <button class="dropdown-item" type="button"
+                onclick="location.href=''">
+          マイページ
+        </button>
+        <div class="dropdown-divider"></div>
+        <button form="logout-button" class="dropdown-item" type="submit">
+          ログアウト
+        </button>
+      </div>
+    </li>
+    <form id="logout-button" method="POST" action="{{ route('logout') }}"> {{--この行を編集--}}
+      @csrf
+    </form>
+    @endauth
   </ul>
 </div>
 </nav>
