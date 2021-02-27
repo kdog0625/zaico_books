2e734f7505745684e9534de7c84c3d0d2931e7df
kdog0625 <kdoooog34@gmail.com>
2021-02-27 11:45:19 +0900

    仮の詳細ページ表示

--- laravel/resources/views/tweets/show.blade.php
+++ laravel/resources/views/tweets/show.blade.php
@@ -6,6 +6,17 @@
 
 <div class="wrapper">
   @include('common/nav')
+  <div class="container">
+    <div class="main-content">
+        <tr>
+          <th><a class="dropdown-item" href="{{ route('tweets.show', ['tweet' => $tweet]) }}">{{ $tweet->zaico_number }}</a></th>
+          <th>{{ $tweet->zaico_name }}</th>
+          <th>{{ $tweet->zaico_count }}</th>
+          <th>{{ $tweet->category }}</th>
+          <th>{{ $tweet->zaico_image }}</th>
+        </tr>
+    </div>
+  </div>
   @include('common/footer')
 </div>  
 
